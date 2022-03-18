8cf150394e70595417806e20c8a03edfa91c3710
mio <yuchang@simfinity.cn>
2022-03-18 18:16:12 +0800

    backup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Home from "./component/Home";
+// import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Mio connect! hhh?
+          
         </p>
         <a
           className="App-link"
@@ -17,7 +20,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Home />
     </div>
   );
 }
